002dddbb5c663a22dd20a746c6e57b516de3fbc6
Vince014 <vince014@cougars.csusm.edu>
2016-02-10 21:27:57 -0800

    Comments and rating
    Added the comments and rating widgets as specified in the assignment.

--- app/src/main/java/com/example/sivin/spinner/BeerExpert.java
+++ app/src/main/java/com/example/sivin/spinner/BeerExpert.java
@@ -3,10 +3,16 @@
  */
 
 package com.example.sivin.spinner;
+import android.widget.EditText;
+import android.widget.RatingBar;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BeerExpert {
+    protected float[] starRatings = { 0, 0, 0, 0 };
+    protected String[] beerComments = { "comments?", "comments?", "comments?", "comments?" };
+
     List<String> getBrands(String color){
         List<String> brands = new ArrayList<String>();
         if (color.equals("Amber")){
@@ -18,5 +24,57 @@ public class BeerExpert {
             brands.add("Gout Stout");
         }
         return brands;
+    }
+
+    void loadUserFeedback(String color, RatingBar rating, EditText comments){
+        float userRating;
+        String userComments;
+
+        if (color.equals("Light")){
+            userRating = starRatings[0];
+            userComments = beerComments[0];
+            rating.setRating(userRating);
+            comments.setText(userComments);
+        }
+        if(color.equals("Amber")){
+            userRating = starRatings[1];
+            userComments = beerComments[1];
+            rating.setRating(userRating);
+            comments.setText(userComments);
+        }
+        if (color.equals("Brown")){
+            userRating = starRatings[2];
+            userComments = beerComments[2];
+            rating.setRating(userRating);
+            comments.setText(userComments);
+        }
+        if(color.equals("Dark")){
+            userRating = starRatings[3];
+            userComments = beerComments[3];
+            rating.setRating(userRating);
+            comments.setText(userComments);
+        }
+    }
+
+    void saveUserFeedback(String color, RatingBar rating, EditText comments) {
+        float userRating = rating.getRating();
+        String userComments = comments.getText().toString();
+
+        if (color.equals("Light")) {
+            starRatings[0] = userRating;
+            beerComments[0] = userComments;
+        }
+        if (color.equals("Amber")) {
+            starRatings[1] = userRating;
+            beerComments[1] = userComments;
+        }
+        if (color.equals("Brown")) {
+            starRatings[2] = userRating;
+            beerComments[2] = userComments;
+        }
+        if (color.equals("Dark")) {
+            starRatings[3] = userRating;
+            beerComments[3] = userComments;
+        }
     }
 }
--- app/src/main/java/com/example/sivin/spinner/MainActivity.java
+++ app/src/main/java/com/example/sivin/spinner/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.sivin.spinner;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -19,51 +17,44 @@ import java.util.List;
 
 public class MainActivity extends Activity {
     private BeerExpert expert = new BeerExpert();
+    protected TextView brands;
+    protected Spinner color;
+    protected RatingBar rating;
+    protected EditText comments;
     private String[] beerColors = { "Light", "Amber", "Brown", "Dark" };
-    public TextView brands;
-    public Spinner color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Get reference to the TextView
+        //Get reference to the TextView, rating bar and comments field
         brands = (TextView) findViewById(R.id.textView_brands);
+        rating = (RatingBar) findViewById(R.id.ratingBar);
+        comments = (EditText) findViewById(R.id.editText_Comment);
 
-        //Get reference to the Spinner...
+        //Get reference to the Spinner and populate it with an adapter
         color = (Spinner) findViewById(R.id.spinner_beerColors);
-        //.... and populate it with an Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, beerColors);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         color.setAdapter(adapter);
 
-        //Get reference to the button & set a listener
+        //Get reference to the beer color picker button & set a listener
         Button button_ChooseBeer = (Button) findViewById(R.id.button_findBeer);
         button_ChooseBeer.setOnClickListener(updateSuggestions);
 
-        //Get reference to the selected item in the spinner
-        /*String beerType = String.valueOf(color.getSelectedItem());
-        Log.d("Color", beerType);
-
-        //Get recommendations from the BeerExpert class
-        List<String> brandsList = expert.getBrands(beerType);
-        StringBuilder brandsFormatted = new StringBuilder();
-        for (String brand : brandsList) {
-            brandsFormatted.append(brand).append('\n');
-        }
-
-        //Display the selected item
-        brands.setText(brandsFormatted);*/
-
+        //Get reference to the comments button & set a listener
+        Button button_Save = (Button) findViewById(R.id.button_Save);
+        button_Save.setOnClickListener(saveUserInput);
     }
 
-    //Call when the user clicks the button
+    //Call when the user clicks the button --- Selects a color of beer
     public View.OnClickListener updateSuggestions = new View.OnClickListener(){
         public void onClick(View view) {
             //Get reference to the selected item in the spinner
             String beerType = String.valueOf(color.getSelectedItem());
+            Log.d("OnClick", "Selected beer color");
 
             //Get recommendations from the BeerExpert class
             List<String> brandsList = expert.getBrands(beerType);
@@ -72,9 +63,26 @@ public class MainActivity extends Activity {
                 brandsFormatted.append(brand).append('\n');
             }
 
-            //Display the selected item
+            //Display the selected items
             brands.setText(brandsFormatted);
-        }
 
+            //Get the user-made star rating and comments for the selected beer
+            expert.loadUserFeedback(beerType, rating, comments);
+        }
     };
+
+    //Call when the user clicks the button --- Selects a color of beer
+    public View.OnClickListener saveUserInput = new View.OnClickListener(){
+        //The thing about this is that it's possible to highlight a different beer
+        //without pressing "Find Beer" and save the information there.
+        public void onClick(View view) {
+            //Get reference to the selected item in the spinner
+            String beerType = String.valueOf(color.getSelectedItem());
+            Log.d("OnClick", "Selected beer color");
+
+            //Get the user-made star rating and comments for the selected beer
+            expert.saveUserFeedback(beerType, rating, comments);
+        }
+    };
+
 }
